adba2421c78415023d9c68b0c07721a0f99c9d50
itishn1k <kayrat1224mamataev@gmail.com>
2022-11-28 02:27:52 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework_3_6/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.homework_3_6
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.homework_3_6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.image, ImageFragment()).commit()
+
+        }
+    }
+}
